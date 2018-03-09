8b829b7d1d5db183339f9beee46dc8325ab4249c
qinmenghuan <1250397216@qq.com>
2018-03-09 09:20:33 +0800

    goBang

--- src/page/goBang/goBang.js
+++ src/page/goBang/goBang.js
@@ -5,6 +5,14 @@
 
 var cellNumber=10;// 横排多少个棋格
 var boardWidth=0; //游戏
+var gameCanvas=null;  // 游戏
+var pieceImg_white,
+  pieceImg_black,
+  cellWidth,
+  boardMargin=25,
+  colorIsBlack=true,
+  pieceIndexList=[];
+
 
 export default {
 	name: 'GoBang',
@@ -12,14 +20,10 @@ export default {
 		return {
       gameCanvas:null,
       gameCtx:null,
-      cellWidth:null,
       boardWidth:0
 		}
 	},
-	// 创建后login
-	created() {
-	  this.init();
-	},
+
   // 面板组件加载后
   mounted(){
     this.init();
@@ -27,58 +31,72 @@ export default {
 	methods: {
     // 初始化
     init(){
-      this.gameCanvas=document.getElementById("gameboard");
+      let self=this;
+      self.gameCanvas=document.getElementById("gameboard");
       boardWidth = window.innerWidth-40;
       this.gameCanvas.width=boardWidth;
       this.gameCanvas.height=boardWidth;
       this.gameCtx=this.gameCanvas.getContext("2d");
 
       // 加载图片资源
-      var pieceImg=new Image();
-      pieceImg.onload=function () {
-        // 初始化游戏对象
-        initSprites(img);
-        // 设置背景颜色
-        ctx.fillStyle=s_bg.color;
-        //
-        okbtn={
-          x:(width-s_buttons.Ok.width)/2,
-          y:height-200,
-          width:s_buttons.Ok.width,
-          height:s_buttons.Ok.height
-        };
-        run();
+      pieceImg_white=new Image();
+      pieceImg_white.onload=function () {
+        // 渲染界面
+        self.renderGame();
       }
-      pieceImg.src=require('../../assets/piece_white.png');
+      pieceImg_white.src=require('../../assets/piece_white.png');
+      pieceImg_black=new Image();
+      pieceImg_black.src=require('../../assets/piece_black.png');
 
-      // 渲染界面
-      this.renderGame();
+      // 监听触摸事件
+      document.addEventListener("touchstart",self.touchOnpress);
     },
     runGame(){
-
     },
     // 渲染页面
     renderGame(){
       // 设置填充背景颜色
-      this.gameCtx.fillStyle="#ffe198";
+      this.gameCtx.fillStyle="#ce5e08";
       this.gameCtx.fillRect(0,0,boardWidth,boardWidth);
+      this.gameCtx.fillStyle="#ffe198";
+      this.gameCtx.fillRect(boardMargin,boardMargin,boardWidth-2*boardMargin,boardWidth-2*boardMargin);
 
       // 初始化网格
-      this.cellWidth=boardWidth/cellNumber;
+      cellWidth=(boardWidth-2*boardMargin)/cellNumber;
       this.gameCtx.lineWidth = 1;
       this.gameCtx.strokeStyle = '#532d06';
       // 循环遍历网格
-      for(let i=0;i<cellNumber;i++){
+      for(let i=0;i<cellNumber+1;i++){
         // 横向线
-        this.gameCtx.moveTo(0,i*this.cellWidth);
-        this.gameCtx.lineTo(boardWidth,i*this.cellWidth);
+        this.gameCtx.moveTo(boardMargin,boardMargin+i*cellWidth);
+        this.gameCtx.lineTo(boardWidth-boardMargin,boardMargin+i*cellWidth);
         // 纵向线
-        this.gameCtx.moveTo(i*this.cellWidth,0);
-        this.gameCtx.lineTo(i*this.cellWidth,boardWidth);
+        this.gameCtx.moveTo(i*cellWidth+boardMargin,boardMargin);
+        this.gameCtx.lineTo(i*cellWidth+boardMargin,boardWidth-boardMargin);
       }
       this.gameCtx.stroke();
+    },
+    // 触摸事件
+    touchOnpress(evt){
+      var mx=evt.offsetX,my=evt.offsetY;
+      // 兼容手机端
+      if(mx==null||my==null){
+        mx=evt.touches[0].clientX;
+        my=evt.touches[0].clientY;
+      }
+      console.log("mx:",mx,"my:",my);
+      // 校验落子在棋盘外
+      // 落子
+      let pieceMargin=boardMargin-cellWidth/2;
+      let xCellIndex=parseInt((mx-pieceMargin)/cellWidth);
+      let yCellIndex=parseInt((my-pieceMargin)/cellWidth);
+      // 校验
 
-
+      pieceIndexList.push({xCellIndex:xCellIndex,yCellIndex:yCellIndex});
+      let pieceWidth=cellWidth;// 棋子宽度
+      let pieceImage=colorIsBlack?pieceImg_black:pieceImg_white;
+      this.gameCtx.drawImage(pieceImage,pieceMargin+xCellIndex*cellWidth,pieceMargin+yCellIndex*cellWidth,pieceWidth,pieceWidth);
+      colorIsBlack=!colorIsBlack;
     },
 		// 登录
 		login: function(event) {
